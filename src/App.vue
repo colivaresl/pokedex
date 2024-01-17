4eb057f58cdd64e07200f7f3a2cdb9f0512f719f
Carlos <colivares@ucclog.com>
2024-01-17 11:57:15 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Pokedex logo" src="./assets/pokedex_logo.png">
+
+    <lista-pokemon></lista-pokemon>
+  </div>
+</template>
+
+<script>
+import ListaPokemon from './components/ListaPokemonComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ListaPokemon
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ListaPokemonComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>aaaa</h1>
+    </div>
+</template>
+
+<script>
+
+
+
+
+export default {
+    name: 'PokedexListaPokemon',
+
+    data() {
+        return {
+             
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
